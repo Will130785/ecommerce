ac05c0c9da5d7cfe651a74233f2dc3b69d76548a
willconstable <will_constable@msn.com>
2020-08-24 20:55:25 +0100

    Create React app and setup frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,56 @@
+export default {
+    products: [
+        {
+        name: "Slim Shirt",
+        category: "Shirts",
+        image: "/images/d1.jpg",
+        price: 50,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: "Fit Shirt",
+        category: "Shirts",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.2,
+        numReviews: 10
+    },
+    {
+        name: "Best Pants",
+        category: "Pants",
+        image: "/images/d1.jpg",
+        price: 70,
+        brand: "Nike",
+        rating: 4,
+        numReviews: 10
+    },
+    {
+        name: "Slim Shirt",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: "Slim Shirt",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: "Slim Shirt",
+        image: "/images/d1.jpg",
+        price: 60,
+        brand: "Nike",
+        rating: 4.5,
+        numReviews: 10
+    }
+]
+
+}
